41bb86b1db4b42ae1de4f4f954fafc4aca1fc31b
Chrysovalantis Anastasiou <chrysovalantis.anastasiou@gmail.com>
2021-05-05 15:26:16 -0700

    JoinFederation tests

--- projectmetis/controller/controller_service_test.cc
+++ projectmetis/controller/controller_service_test.cc
@@ -5,15 +5,69 @@
 namespace projectmetis {
 namespace {
 
-TEST(ControllerService, JoinFederation) {
+TEST(ControllerService, JoinFederationEmptyRequest) /* NOLINT */ {
   auto service = controller::New();
 
   JoinFederationRequest req;
   JoinFederationResponse res;
 
-  service->JoinFederation(nullptr, &req, &res);
+  auto status = service->JoinFederation(nullptr, &req, &res);
 
+  EXPECT_FALSE(status.ok());
+}
+
+TEST(ControllerService, JoinFederationHasHostButEmptyPort) /* NOLINT */ {
+  auto service = controller::New();
+
+  JoinFederationRequest req;
+  req.mutable_learner_entity()->set_hostname("localhost");
+  JoinFederationResponse res;
+
+  auto status = service->JoinFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+}
+
+TEST(ControllerService, JoinFederationHasPortButEmptyHost) /* NOLINT */ {
+  auto service = controller::New();
+
+  JoinFederationRequest req;
+  req.mutable_learner_entity()->set_port(1991);
+  JoinFederationResponse res;
+
+  auto status = service->JoinFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+}
+
+TEST(ControllerService, JoinFederationLearnerServiceNotReachable) /* NOLINT */ {
+  auto service = controller::New();
+
+  JoinFederationRequest req;
+  req.mutable_learner_entity()->set_hostname("localhost");
+  req.mutable_learner_entity()->set_port(1991);
+  JoinFederationResponse res;
+
+  auto status = service->JoinFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+}
+
+TEST(ControllerService, JoinFederationSuccess) /* NOLINT */ {
+  auto service = controller::New();
+
+  // TODO(canastas): start a temporary service here
+
+  JoinFederationRequest req;
+  req.mutable_learner_entity()->set_hostname("localhost");
+  req.mutable_learner_entity()->set_port(1991);
+  JoinFederationResponse res;
+
+  auto status = service->JoinFederation(nullptr, &req, &res);
+
+  EXPECT_TRUE(status.ok());
   EXPECT_TRUE(res.ack().status());
+  EXPECT_FALSE(res.auth_token().empty());
 }
 
 }  // namespace
