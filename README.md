18f1d146a314e81b11a86b16e54706b79bb5d942
Dimitris Stripelis <stripeli@isi.edu>
2021-05-12 19:09:01 +0000

    Merged in feature/ControllerJoinLeaveFederationRPC (pull request #3)
    Feature/ControllerJoinLeaveFederationRPC
    
    * Implementation of Controller's Join and Leave Federation grpc endpoints. Pending throrough testing and mock localhost server.
    
    * Finalizing Join and Leave Federation controller gRPC endpoints along with TEST cases. Pending tasks are validating the existence of the remote learner on the controller end and initializing a learner micro service in the TEST
    
    * Rearranged null pointer if condition checkers.
    
    * Fixing return Status from Controller endpoints.
    
    * Added empty lines before comments in cotnroller_service.cc
    
    Approved-by: Chrysovalantis Anastasiou

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
+## Prerequisites
+- Install googletest (MacOS as `brew install googletest`)
+- Install protobuf (MacOS as `brew install protobuf`)
+
+
 ## Trello UI
 https://trello.com/b/bYLUYqGK/metis-v01
+
+## How to create a pull request on bitbucket.org!
+1. create a new branch using any of the four branch types: `release/, hotfix/, feature/, bugfix/`     
+   e.g., `git checkout -b feature/<branch_suffix>`
+2. make all the necessary changes/edits you want on the project and commit
+3. push the local branch to the remote
+4. navigate on the bitbucket environment, find your branch (left sidebar -> branches -> your_new_branch)
+5. create a pull request and assign reviewers
+6. if changes need to be made on the request, then work on your local branch, commit and push
+7. reviewers will see the new edits and if approved then they will merge the request
+
+##### Recommendation on how to identify your branch type:
+You need to figure out the type of your patch. For instance, if a bunch of modules are bundled together 
+then this is a release; if your patch performs minor edits/alterations to an existing approach but the 
+functionality and the logic of the codebase remains the same, then you can assign it as a hotfix; If you
+introduce an entirely new implementation, functionality then feature is the best option; if you fix severe
+bugs then of course bugfix is where you need to push.  
+
--- projectmetis/controller/controller_service_test.cc
+++ projectmetis/controller/controller_service_test.cc
@@ -16,16 +16,23 @@ TEST(ControllerService, JoinFederationEmptyRequest) /* NOLINT */ {
   EXPECT_FALSE(status.ok());
 }
 
-TEST(ControllerService, JoinFederationHasHostButEmptyPort) /* NOLINT */ {
+TEST(ControllerService, JoinFederationHasHostButNegativePort) /* NOLINT */ {
+  /**
+   * In proto3 all messages have a default value. For the case of an int_32 type
+   * the default value is 0. Since 0 is a valid port even though unconventional
+   * for a service to run, we need to validate that no negative value is passed.
+   */
   auto service = controller::New();
 
   JoinFederationRequest req;
   req.mutable_learner_entity()->set_hostname("localhost");
+  req.mutable_learner_entity()->set_port(-1234);
   JoinFederationResponse res;
 
   auto status = service->JoinFederation(nullptr, &req, &res);
 
   EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
 }
 
 TEST(ControllerService, JoinFederationHasPortButEmptyHost) /* NOLINT */ {
@@ -38,6 +45,22 @@ TEST(ControllerService, JoinFederationHasPortButEmptyHost) /* NOLINT */ {
   auto status = service->JoinFederation(nullptr, &req, &res);
 
   EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
+}
+
+TEST(ControllerService, JoinFederationHasNegativeDatasetSpec) /* NOLINT */ {
+  auto service = controller::New();
+
+  JoinFederationRequest req;
+  req.mutable_learner_entity()->set_hostname("localhost");
+  req.mutable_learner_entity()->set_port(1991);
+  req.mutable_local_dataset_spec()->set_num_training_examples(-1);
+  JoinFederationResponse res;
+
+  auto status = service->JoinFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
 }
 
 TEST(ControllerService, JoinFederationLearnerServiceNotReachable) /* NOLINT */ {
@@ -46,11 +69,13 @@ TEST(ControllerService, JoinFederationLearnerServiceNotReachable) /* NOLINT */ {
   JoinFederationRequest req;
   req.mutable_learner_entity()->set_hostname("localhost");
   req.mutable_learner_entity()->set_port(1991);
+  req.mutable_local_dataset_spec()->set_num_training_examples(100);
   JoinFederationResponse res;
 
   auto status = service->JoinFederation(nullptr, &req, &res);
 
   EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
 }
 
 TEST(ControllerService, JoinFederationSuccess) /* NOLINT */ {
@@ -61,13 +86,94 @@ TEST(ControllerService, JoinFederationSuccess) /* NOLINT */ {
   JoinFederationRequest req;
   req.mutable_learner_entity()->set_hostname("localhost");
   req.mutable_learner_entity()->set_port(1991);
+  req.mutable_local_dataset_spec()->set_num_training_examples(100);
   JoinFederationResponse res;
 
   auto status = service->JoinFederation(nullptr, &req, &res);
 
   EXPECT_TRUE(status.ok());
   EXPECT_TRUE(res.ack().status());
+  EXPECT_FALSE(res.learner_id().empty());
   EXPECT_FALSE(res.auth_token().empty());
+}
+
+TEST(ControllerService, LeaveFederationEmptyRequest) /* NOLINT */ {
+  auto service = controller::New();
+
+  LeaveFederationRequest req;
+  LeaveFederationResponse res;
+
+  auto status = service->LeaveFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+}
+
+TEST(ControllerService, LeaveFederationEmptyLearnerID) /* NOLINT */ {
+  auto service = controller::New();
+
+  LeaveFederationRequest req;
+  req.set_auth_token("1234");
+  LeaveFederationResponse res;
+
+  auto status = service->LeaveFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
+}
+
+TEST(ControllerService, LeaveFederationEmptyAuthToken) /* NOLINT */ {
+  auto service = controller::New();
+
+  LeaveFederationRequest req;
+  req.set_learner_id("localhost");
+  LeaveFederationResponse res;
+
+  auto status = service->LeaveFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
+}
+
+TEST(ControllerService, LeaveFederationNonRegisteredLearner) /* NOLINT */ {
+  auto service = controller::New();
+
+  LeaveFederationRequest req;
+  req.set_learner_id("localhost");
+  req.set_auth_token("1234");
+  LeaveFederationResponse res;
+
+  auto status = service->LeaveFederation(nullptr, &req, &res);
+
+  EXPECT_FALSE(status.ok());
+  EXPECT_FALSE(res.ack().status());
+}
+
+TEST(ControllerService, LeaveFederationSucess) /* NOLINT */ {
+  /**
+   * First we need to register the new learner with the controller/federation,
+   * hence the first JoinFederationRequest and then we need to ask from the
+   * controller to exclude the learner from the federation.
+   */
+  auto service = controller::New();
+
+  // TODO(canastas): start a temporary service here
+
+  JoinFederationRequest join_req;
+  join_req.mutable_learner_entity()->set_hostname("localhost");
+  join_req.mutable_learner_entity()->set_port(1991);
+  join_req.mutable_local_dataset_spec()->set_num_training_examples(100);
+  JoinFederationResponse join_res;
+
+  auto join_status = service->JoinFederation(nullptr, &join_req, &join_res);
+
+  LeaveFederationRequest leave_req;
+  leave_req.set_learner_id(join_res.learner_id());
+  leave_req.set_auth_token(join_res.auth_token());
+  LeaveFederationResponse leave_res;
+  auto leave_status = service->LeaveFederation(nullptr, &leave_req, &leave_res);
+
+  EXPECT_TRUE(leave_status.ok());
+  EXPECT_TRUE(leave_res.ack().status());
 }
 
 }  // namespace
