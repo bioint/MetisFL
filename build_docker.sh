bcb8b9480642056014ae99e025d46fb244d70d60
Panagiotis Kyriakis <pvkiriakis@gmail.com>
2023-06-17 22:32:30 +0000

    updated dockerfiles; added py37

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+IMAGE_NAME=$1
+IMG_LS=$(docker image list | grep $IMAGE_NAME) 
+if [[ $IMG_LS == "" ]]; then
+  echo "Image $IMAGE_NAME not found. Pulling.."
+  docker pull $IMAGE_NAME
+fi
 
-CID=$(docker run -dit -v .:/metisfl nevron/builder_ubuntu_x86_64_py39)
+CID=$(docker run -dit -v .:/metisfl nevron/$IMAGE_NAME)
+
+echo "Waiting for container to start..."
+until [ "`docker inspect -f {{.State.Running}} $CID`"=="true" ]; do
+    sleep 0.1;
+done;
+echo "Container started. Building"
 
 # Run build
 docker exec -it $CID /bin/bash -c build.sh
--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+docker build - < DockerfileUbuntuPY37 -t nevron/builder_ubuntu_x86_64_py37
 docker build - < DockerfileUbuntuPY38 -t nevron/builder_ubuntu_x86_64_py38
 docker build - < DockerfileUbuntuPY39 -t nevron/builder_ubuntu_x86_64_py39
 docker build - < DockerfileUbuntuPY310 -t nevron/builder_ubuntu_x86_64_py310
