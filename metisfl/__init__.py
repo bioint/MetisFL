3c6ff86bbf68aa9b95268bbaa01020f0ee080468
Panagiotis Kyriakis <pvkiriakis@gmail.com>
2023-09-06 09:08:04 +0000

    added global config in metisfl.common

--- metisfl/__init__.py
+++ metisfl/__init__.py
@@ -7,4 +7,6 @@ from . import proto
 from . import common
 from . import driver
 
+from metisfl.common.config import *
+
 __all__ = ("controller", "learner", "proto", "common", "driver")
---
+++ metisfl/common/config.py
@@ -0,0 +1,6 @@
+
+"""Configuration for the MetisFL package."""
+
+from loguru import logger
+
+logger.level("INFO", color="<white>")
